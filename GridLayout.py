034d8b93e96db016e3faa7391aee1f066fbc6f51
Ruchika1706 <luthraruchika19@gmail.com>
2017-04-03 22:21:50 +0530

    Grid Layout

---
+++ GridLayout.py
@@ -0,0 +1,14 @@
+from Tkinter import *
+root = Tk()
+label1 = Label(root, text = "Label1")
+label2 = Label(root, text = "Label2")
+
+#Text Fields are called Entries
+entry1 = Entry(root)
+entry2 = Entry(root)
+
+label1.grid(row = 0, column = 0)
+label2.grid(row = 1, column = 0)
+entry1.grid(row = 0, column = 1)
+entry2.grid(row = 1, column = 1)
+root.mainloop()
