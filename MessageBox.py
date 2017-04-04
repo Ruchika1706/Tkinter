064a5ab77b53ecf22c32bc7d6bb823901be71737
Ruchika1706 <luthraruchika19@gmail.com>
2017-04-04 15:04:37 +0530

    MessageBox

--- MessageBox.py
+++ MessageBox.py
@@ -4,4 +4,9 @@ import tkMessageBox
 
 root = Tk()
 tkMessageBox.showinfo("title", "This is awesome")
+response = tkMessageBox.askquestion("Question1","Do you like coffee?")
+if response == "yes":
+    print("Cool")
+elif response == "no":
+    print("Why not?")
 root.mainloop()
