eaf8864db568212a49ae90e4fe944f52c57c73df
Ruchika1706 <luthraruchika19@gmail.com>
2017-04-04 15:05:19 +0530

    Plain/Ask Question Message box

--- MessageBox.py
+++ MessageBox.py
@@ -3,7 +3,9 @@ from Tkinter import *
 import tkMessageBox
 
 root = Tk()
+#Plain Message Box
 tkMessageBox.showinfo("title", "This is awesome")
+#Ask question MessageBox
 response = tkMessageBox.askquestion("Question1","Do you like coffee?")
 if response == "yes":
     print("Cool")
